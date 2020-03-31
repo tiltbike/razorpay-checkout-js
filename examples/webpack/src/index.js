b8a559ad9d95c0ee6f375f009772805ef569fec8
Daksh Shah <dakshshah96@gmail.com>
2020-03-31 21:26:12 +0530

    add webpack example

---
+++ examples/webpack/src/index.js
@@ -0,0 +1,27 @@
+import './styles.css';
+import { loadCheckout } from '../../../src';
+
+(async () => {
+  // setup DOM
+  const rootNode = document.getElementById('root');
+  const form = document.createElement('form');
+  const button = document.createElement('button');
+  button.innerText = 'Pay';
+  form.appendChild(button);
+  rootNode.appendChild(form);
+
+  // setup Razorpay checkout.js
+  const razorpay = await loadCheckout({
+    // change below key to your live Razorpay key
+    key: 'rzp_test_ThB8MHSwsn2cQPU',
+    amount: 10000,
+    name: 'Razorpay',
+    description: 'Testing checkout.js',
+  });
+
+  // handle form submit
+  form.addEventListener('submit', async (e) => {
+    razorpay.open();
+    e.preventDefault();
+  });
+})();
